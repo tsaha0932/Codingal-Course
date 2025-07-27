1ab2545b4955c27dd8fd5a0eac99bd6f0cae62fa
tsaha0932 <tistasaha23@gmail.com>
2025-07-27 12:57:10 +0530

    initial commit

---
+++ M7/CW Activity/L1-A1.py
@@ -0,0 +1,16 @@
+#Printing a greeting message
+print("Hello, world!")
+
+#Printing my name
+print("My name is Tista.")
+
+#Printing a fun fact about myself
+print("I love dancing.")
+
+#Performing and printing a simple calculation
+print("The result of 3 + 5 is:", 3 + 5)
+
+
+'''
+This is multiple line comments
+'''
---
+++ M7/CW Activity/L1-A4.py
@@ -0,0 +1,12 @@
+#Print a sentence
+print("Welcome to the world of programming!")
+
+#Print a number
+print(4)
+
+#Next Line after print
+print("Hello Python \n\n\nThis sentence is coming in the new line")
+
+#Print all the keywords present in Python print("Keywords in Python are...")
+import keyboard
+print(keyboard.kwlist)
