08d185a195ab43e8aaf69f7c98af963d30502bb7
tsaha0932 <tistasaha23@gmail.com>
2025-08-02 13:01:34 +0530

    initial commit

--- M7/CW Activity/L6-A1.py
+++ M7/CW Activity/L6-A1.py
@@ -16,22 +16,4 @@ for i in range(n):
     board.forward(100)
     board.left(360/n)
 
-import turtle
-
-# creating canvas
-turtle.Screen().bgcolor("pink")
-
-turtle.Screen().setup(500, 500)
-
-turtle.title("Welcome to Turtle Window")
-
-# turtle object creation
-board= turtle.Turtle()
-
-n = 6
-# creating a square
-for i in range(n):
-    board.forward(100)
-    board.left(360/n)
-
 turtle.done()
