794133bbf43facaff3255405a4f8d2d1d5441c70
tsaha0932 <tistasaha23@gmail.com>
2025-08-22 20:22:53 +0530

    initial commit

---
+++ M8/HW Activity/L2-HW.py
@@ -0,0 +1,9 @@
+my_tuple = (1, 2, 3, 'a', 'b', 4.5)
+
+my_list = list(my_tuple)
+
+print(f"Original tuple: {my_tuple}")
+print(f"Type of original tuple: {type(my_tuple)}")
+
+print(f"Converted list: {my_list}")
+print(f"Type of converted list: {type(my_list)}")
