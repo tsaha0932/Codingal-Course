aecba4bae7cd6f717ea801fad5c8144cf3686572
tsaha0932 <tistasaha23@gmail.com>
2025-10-06 17:46:18 +0530

    initial commit

---
+++ M10/HW Activity/L2-HW.py
@@ -0,0 +1,17 @@
+def myfunction(n):
+    for i in range(0, n + 1):
+        print("First Loop")
+    j = 1
+    while j <= n + 1:
+        print("Second Loop", j)
+        j = j * 2
+
+    for i in range(0, 100):
+        print("Third loop")
+n = 10
+myfunction(n)
+print("\nTime Complexity of Each Part:")
+print("First Loop: O(n)")
+print("Second Loop: O(log n)")
+print("Third Loop: O(1)")
+print("Total Time Complexity: O(n)")
--- M10/HW Activity/L3-HW.py
+++ M10/HW Activity/L3-HW.py
@@ -2,19 +2,22 @@ def myfunction1(n):
     if n <= 0:
         return
 
-    for i in range(0, n + 1):
+    for i in range(0, int(n) + 1):
         print("Codingal")
 
     myfunction1(n / 2)
     myfunction1(n / 3)
 
-myfunction1(10)
-
 def myfunction2(n):
     if n <= 1:
         return
+    
     print("Codingal")
-
     myfunction2(n - 1)
 
+# Run both functions
+print("Output of myfunction1:")
+myfunction1(10)
+
+print("\nOutput of myfunction2:")
 myfunction2(10)
