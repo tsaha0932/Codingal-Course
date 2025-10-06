128f2e6fb74d506623410806891962cd64e7918e
tsaha0932 <tistasaha23@gmail.com>
2025-10-06 18:26:39 +0530

    initial commit

---
+++ M7/HW Activity/L4-HW.py
@@ -0,0 +1,15 @@
+def is_armstrong(number):
+
+    num_str = str(number)
+    num_digits = len(num_str)
+   
+    total = sum(int(digit) ** num_digits for digit in num_str)
+
+    return total == number
+
+num = int(input("Enter a number: "))
+
+if is_armstrong(num):
+    print(f"{num} is an Armstrong number.")
+else:
+    print(f"{num} is NOT an Armstrong number.")
