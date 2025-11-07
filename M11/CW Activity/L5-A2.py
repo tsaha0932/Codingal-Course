ab12e64ff0ca5d48553b14d58f048e0c1477a347
tsaha0932 <tistasaha23@gmail.com>
2025-11-07 19:07:33 +0530

    initial commit

---
+++ M11/CW Activity/L5-A2.py
@@ -0,0 +1,25 @@
+#Write a program to divide 2 numbers without using division operator
+def divide(ourdividend, ourDivisor):
+    sign = (-1 if((ourdividend < 0) ^ (ourDivisor < 0)) e;se 1)
+    ourdividend = abs(ourdividend)
+    ourDivisor = abs(ourDivisor)
+
+    quotientNumber = 0
+    tempNumber = 0
+
+    for i in range(31, -1, -1):
+        if(tempNumber + (ourDivisor << i) <= ourdividend): #0+(3*2**2) <= 13
+            tempNumber += ourDivisor << i # 0+(3<<2) = 3*2**2 = 12
+            quotientNumber |= 1 << i #0 or 1<<2 = 000 or 100 = 100 = 4
+    
+    if sign == -1:
+        quotientNumber = -quotientNumber
+    return quotientNumber
+
+
+a = int(input("Enter a for a/b: ")) #dividend
+b = int(input("Enter b for a/b: ")) #divisor
+print("Result of", a, "/", b, "is", divide(a, b))
+
+#a = 13 = 1101
+#b = 3 = 0011
