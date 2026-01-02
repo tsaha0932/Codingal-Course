c16ecf4bec6b042f162f4e8d5196ed64b7fcd97d
tsaha0932 <tistasaha23@gmail.com>
2026-01-02 19:12:37 +0530

    initial commit

---
+++ M14/CW Activity/L3.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+
+#Student data
+students_names = ["Sanjay", "Rahul", "Karan", "Wasim", "Ramesh", "Ajay", "Sartaj", "Priya"]
+students_marks = [35, 50, 20, 45, 25, 40, 25, 40]
+
+#Calculate percentage (assumming total marks = 50)
+marks_perc = [(marks / 50) * 100 for marks in students_marks]
+
+#Line chart for raw marks
+def marks_line_chart():
+    plt.plot(students_names, students_marks, marker='o', linestyle='-', color='blue')
+    plt.title("Students Marks Graph")
+    plt.xlabel("Student Names")
+    plt.ylabel("Markss out of 50")
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+#Bar chart for percentage
+def percentage_bar_chart():
+    plt.bar(students_names, marks_perc, color='green')
+    plt.title("Students Percentage Graph")
+    plt.xlabel("Student Names")
+    plt.ylabel("Percentage (%)")
+    plt.ylim(0, 100)
+    plt.tight_layout()
+    plt.show()
+
+#Run both charts
+marks_line_chart()
+percentage_bar_chart()
